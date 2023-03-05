db067fdcfadad31af3654c54e7c8b375a639aaa4
octopus6677 <octopus6677@163.com>
2023-03-05 13:03:44 +0800

    feat: message box样式

--- src/views/hik/font/index.vue
+++ src/views/hik/font/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="app-container">
+    <!--  动态字体大小  -->
     <h2 :style="{fontSize: fontSize + 'px'}">v-bind绑定style</h2>
     <el-button @click="add">add</el-button>
     <el-button @click="sub">sub</el-button>
+    <!--  message 默认mini  -->
+    <h2>Message Box</h2>
+    <el-button @click="confirm">Message Box</el-button>
+    <el-button size="small">确定</el-button>
+    <el-button>确定</el-button>
+    <div class="text">吃饭</div>
   </div>
 </template>
 
 <script>
+import { MessageBox } from 'element-ui'
 export default {
   name: 'Index',
   data() {
@@ -20,11 +28,48 @@ export default {
     },
     sub() {
       this.fontSize--
+    },
+    confirm() {
+      MessageBox.confirm('这是一段内容', '标题名称', {
+        confirmButtonText: '确定',
+        callback: action => {
+          console.log(1111)
+        }
+      })
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
+.c-c {
+  position: fixed;
+  inset: 0px;
+  z-index: 203;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: center;
+  justify-content: center;
+  overflow: hidden auto;
+}
+/* 等于小于 1600 */
+@media (max-width: 1599px) {
+  .el-message-box__btns .el-button--small {
+    font-size: 12px!important;
+  }
+  .el-message-box__btns .el-button--primary {
+    background-color: #409EFF;
+  }
+}
+/* 大于等于 1600 */
+@media (min-width: 1600px) {
+  .el-message-box__btns .el-button--small {
+    padding: 12px 20px!important;
+    font-size: 14px!important;
+  }
+  .el-message-box__btns .el-button--primary {
+    background-color: #409EFF;
+  }
 
+}
 </style>
