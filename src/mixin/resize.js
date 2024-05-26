52fe1205fa13320d3a6084a1c3f2b77d2081193f
game <izhaohaxishi@gmail.com>
2024-05-26 20:55:51 +0800

    fix: 封装了一个resize函数mixin，实现自定义间隔时间

--- src/mixin/resize.js
+++ src/mixin/resize.js
@@ -1,18 +1,25 @@
 import { debounce } from 'lodash'
 
 export default {
+  data() {
+    return {
+      interval: 100 // 默认值100，想要覆盖在vue文件中重新声明此值即可，会覆盖掉mixin的值
+    }
+  },
   created() {
     // 监听窗口大小变化，并使用防抖函数处理
-    window.addEventListener('resize', this._handleResize)
+    window.addEventListener('resize', this._handleResize(this.interval))
   },
   beforeDestroy() {
     // 在组件销毁前移除resize事件监听
-    window.removeEventListener('resize', this._handleResize)
+    window.removeEventListener('resize', this._handleResize(this.interval))
   },
   methods: {
     // 使用lodash的debounce函数包裹窗口大小变化处理方法
-    _handleResize: debounce(function() {
-      this.handleResize()
-    }, 100)
+    _handleResize: function(interval) {
+      return debounce(() => {
+        this.handleResize()
+      }, interval)
+    }
   }
 }
--- src/views/func/debounce/index.vue
+++ src/views/func/debounce/index.vue
@@ -7,6 +7,11 @@ import resize from '@/mixin/resize'
 export default {
   name: 'Index',
   mixins: [resize],
+  data() {
+    return {
+      interval: 1000
+    }
+  },
   methods: {
     handleResize() {
       console.log('handleResize')
